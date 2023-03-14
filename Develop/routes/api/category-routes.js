21d4d405236653d3d395664c6e866a18fc612cd1
waldenlight <76961678+waldenlight@users.noreply.github.com>
2023-03-14 09:14:38 -0600

    Add post routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -37,6 +37,14 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
